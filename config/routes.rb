fafc35163a9a08b09950a30bbb6cb939fa87fe62
railsbeginner <bartosz.dyja@gmail.com>
2015-09-21 11:57:16 +0200

    Configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
+  root to: 'visitors#index'
+
   devise_for :users
 
   resources :students do
     get :subjects
   end
+  
+  resources :teachers do
+    get :subjects
+  end
+
+  resources :visitors, only: :index
+  
+  get 'report/subjects', to: 'reports#subjects'
 end
